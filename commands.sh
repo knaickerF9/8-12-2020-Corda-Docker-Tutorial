4d7292400e9f97286a641df2f6a4eb6e3bd0295a
David Awad <me@davidawad.com>
2021-03-12 12:24:16 -0500

    make commands a script

--- commands.sh
+++ commands.sh
@@ -1,9 +1,11 @@
-sudo apt-get -qq update
+#!/bin/bash 
+
+sudo apt-get -qq update -y 
 sudo apt-get -qq -y install openjdk-8-jre
-sudo apt install unzip
-sudo apt-get remove docker docker-engine docker.io
-sudo apt install docker.io
-sudo groupadd docker
+sudo apt install unzip -y 
+sudo apt-get remove docker docker-engine docker.io -y 
+sudo apt install docker.io -y 
+sudo groupadd docker 
 sudo usermod -aG docker $USER
 newgrp docker
 sudo curl -L https://github.com/docker/compose/releases/download/1.24.1/docker-compose-`uname -s`-`uname -m` -o /usr/local/bin/docker-compose
