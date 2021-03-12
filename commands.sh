0e79c4278a567fde2b2c3d854ed0e207ee01b007
David Awad <me@davidawad.com>
2021-03-12 12:27:04 -0500

    Update commands.sh

--- commands.sh
+++ commands.sh
@@ -11,3 +11,4 @@ newgrp docker
 sudo curl -L https://github.com/docker/compose/releases/download/1.24.1/docker-compose-`uname -s`-`uname -m` -o /usr/local/bin/docker-compose
 sudo chmod +x /usr/local/bin/docker-compose
 sudo chown $(whoami) -R ./*
+sudo apt install docker-compose -y 
