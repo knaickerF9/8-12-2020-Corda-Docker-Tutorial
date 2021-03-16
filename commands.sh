928900d15ca4faaafcd475073ced39d14f7c7663
David Awad <me@davidawad.com>
2021-03-16 14:09:24 -0400

    Update commands.sh

--- commands.sh
+++ commands.sh
@@ -8,7 +8,5 @@ sudo apt install docker.io -y
 sudo groupadd docker 
 sudo usermod -aG docker $USER
 newgrp docker
-sudo curl -L https://github.com/docker/compose/releases/download/1.24.1/docker-compose-`uname -s`-`uname -m` -o /usr/local/bin/docker-compose
-sudo chmod +x /usr/local/bin/docker-compose
 sudo chown $(whoami) -R ./*
 sudo apt install docker-compose -y 
